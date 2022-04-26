fba8958369b9b4e5ef86375ac0661f6c1bb2f09f
Cama-ro <kam.rybicka@gmail.com>
2022-04-26 20:05:39 +0200

    timer with hooks

---
+++ src/Clock.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import "./Clock.css";
+
+const Clock = (props) => {
+  const [date, setDate] = useState(new Date());
+  const tick = () => setDate(new Date());
+
+  useEffect(() => {
+    let timerID = setInterval(() => {
+      tick();
+    }, 1000);
+
+    return () => {
+      clearInterval(timerID);
+    };
+  });
+
+  return (
+    <div className="clock">
+      <h2>{date.toLocaleTimeString()}</h2>
+      <button onClick={props.toggleClockMethod}>close timer</button>
+    </div>
+  );
+};
+
+export default Clock;
---
+++ src/ToggleButton.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import Clock from "./Clock";
+import "./ToggleButton.css";
+
+class ToggleButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // counterValue: this.props.initValue,
+      showClock: true,
+    };
+  }
+
+  toggleClock = () => {
+    this.setState((prevState) => {
+      return {
+        showClock: !prevState.showClock,
+      };
+    });
+  };
+
+  render() {
+    let clockElement = "";
+
+    if (this.state.showClock) {
+      clockElement = <Clock toggleClockMethod={this.toggleClock} />;
+    } else {
+      clockElement = (
+        <button onClick={this.toggleClock} className="show-clock">
+          show clock
+        </button>
+      );
+    }
+
+    return <div className="toggle-clock">{clockElement}</div>;
+  }
+}
+
+export default ToggleButton;

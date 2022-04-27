e33a0beb1b7cc29b63672da0f09b8a2f7cb63f9a
Cama-ro <kam.rybicka@gmail.com>
2022-04-27 10:10:27 +0200

    modify togglebutton.js

--- src/ToggleButton.js
+++ src/ToggleButton.js
@@ -1,39 +1,63 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Clock from "./Clock";
 import "./ToggleButton.css";
 
-class ToggleButton extends Component {
-  constructor(props) {
-    super(props);
+// class ToggleButton extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      showClock: true,
-    };
-  }
+//     this.state = {
+//       showClock: true,
+//     };
+//   }
 
-  toggleClock = () => {
-    this.setState((prevState) => {
-      return {
-        showClock: !prevState.showClock,
-      };
-    });
+//   toggleClock = () => {
+//     this.setState((prevState) => {
+//       return {
+//         showClock: !prevState.showClock,
+//       };
+//     });
+//   };
+
+//   render() {
+//     let clockElement = "";
+
+//     if (this.state.showClock) {
+//       clockElement = <Clock toggleClockMethod={this.toggleClock} />;
+//     } else {
+//       clockElement = (
+//         <button onClick={this.toggleClock} className="show-clock">
+//           show clock
+//         </button>
+//       );
+//     }
+
+//     return <div className="toggle-clock">{clockElement}</div>;
+//   }
+// }
+
+// export default ToggleButton;
+
+const ToggleButton = () => {
+  const [showClock, setShowClock] = useState(true);
+
+  const toggleClock = () => {
+    setShowClock(!showClock);
   };
 
-  render() {
-    let clockElement = "";
+  let clockElement = "";
 
-    if (this.state.showClock) {
-      clockElement = <Clock toggleClockMethod={this.toggleClock} />;
-    } else {
-      clockElement = (
-        <button onClick={this.toggleClock} className="show-clock">
+  return (
+    <div className="toggle-clock">
+      {showClock ? (
+        <Clock toggleClockMethod={toggleClock} />
+      ) : (
+        <button onClick={toggleClock} className="show-clock">
           show clock
         </button>
-      );
-    }
-
-    return <div className="toggle-clock">{clockElement}</div>;
-  }
-}
+      )}
+    </div>
+  );
+};
 
 export default ToggleButton;
